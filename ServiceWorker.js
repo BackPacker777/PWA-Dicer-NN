712c59afdfdf02e81f780120598771b62800e68b
Howard Bates <hbates@northmen.org>
2017-02-09 13:29:40 -0500

    Fixed some stuff

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,10 +2,11 @@
 
 'use strict';
 
-const CACHE_NAME = 1.0;
+const CACHE_NAME = "1.02";
 let cacheFiles = [
      '/index.html',
      '/public/css/foundation63.min.css',
+     '/public/css/_utility.scss',
      '/public/css/override.css',
      '/public/images/10Dice.png',
      '/public/javascripts/main.js',
@@ -13,8 +14,7 @@ let cacheFiles = [
      '/public/favicons/android-chrome-192x192.png'
 ];
 
-
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
      event.waitUntil(
           caches.open(CACHE_NAME).then((cache) => {
                return cache.addAll(cacheFiles);
@@ -24,12 +24,14 @@ self.addEventListener('install', event => {
 
 self.addEventListener('activate', (event) => {
      event.waitUntil(
-          caches.keys().then(cacheNames => {
+          caches.keys().then(keys => {
                return Promise.all(
-                    cacheNames.map(thisCacheName => {
-                         if (thisCacheName !== CACHE_NAME) {
-                              return caches.delete(CACHE_NAME);
-                         }
+                    keys
+                         .filter((key) => {
+                              return !key.startsWith(CACHE_NAME);
+                    })
+                    .map((key) => {
+                         return caches.delete(key);
                     })
                );
           })
